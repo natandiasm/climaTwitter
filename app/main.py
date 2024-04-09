6908498051d950382bda9040aeb4cb4a69f19299
Natan <dias@speedio.com.br>
2024-04-09 17:54:57 -0300

    change deprecated method on_start

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from dotenv import load_dotenv
@@ -12,21 +13,9 @@ from .utils.twitter import Twitter
 # Load env file
 load_dotenv()
 
-# Create instance of FastAPI
-app = FastAPI(title="climaTwitter",
-              description="an application that consumes the openweathermap and twitter api, connecting them into a "
-                          "simple to use and fast api.",
-              summary="tweet a city's current weather.",
-              docs_url="/doc",
-              version=os.getenv("VERSION")
-              )
 
-# Import the routes that make up version 1
-app.include_router(core.router, prefix='/v1')
-
-
-@app.on_event("startup")
-def setup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     # Start OpenWeatherMap
     owm = OpenWeatherMap()
     owm.start()
@@ -34,6 +23,21 @@ def setup():
     # Start Twitter
     twt = Twitter()
     twt.start()
+    yield
+
+
+# Create instance of FastAPI
+app = FastAPI(title="climaTwitter",
+              description="an application that consumes the openweathermap and twitter api, connecting them into a "
+                          "simple to use and fast api.",
+              summary="tweet a city's current weather.",
+              docs_url="/doc",
+              version=os.getenv("VERSION"),
+              lifespan=lifespan
+              )
+
+# Import the routes that make up version 1
+app.include_router(core.router, prefix='/v1')
 
 
 @app.get("/", description="main endpoint, with it you will know if the api is up and what the api version is.")
